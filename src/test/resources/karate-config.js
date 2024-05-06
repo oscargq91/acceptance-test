c47306b305ecbfa50f0c93f8fa4ddce8239592a1
oegarces <oegarces@bancolombia.com.co>
2024-05-06 18:04:04 -0500

    update

--- src/test/resources/karate-config.js
+++ src/test/resources/karate-config.js
@@ -3,7 +3,7 @@ function fn() {
     karate.configure('readTimeout', 20000);
     karate.configure('ssl', true);
     var baseUrl = karate.properties['baseUrl'] || 'http://localhost:8090/api/v1'
-    var logBaseUrl = karate.properties['logBaseUrl'] || 'http://localhost:8070/api/v1'
+    var logBaseUrl = karate.properties['logBaseUrl'] || 'http://logs-rest:8070/api/v1'
     var healthCheckBaseUrl = karate.properties['healthCheckBaseUrl'] || 'http://health-rest:8050'
 
     return {
